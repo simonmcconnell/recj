a9bb97159adeff9aa3207fc4227b65424cee6ae9
Simon McConnell <22566656+simonmcconnell@users.noreply.github.com>
2024-06-28 08:26:34 +0800

    add oban

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule Recj.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.7"}
+      {:plug_cowboy, "~> 2.7"},
+      {:oban, "~> 2.17"}
     ]
   end
 
