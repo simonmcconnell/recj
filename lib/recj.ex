d4012c06d26e1acb51907c1b5fe7b8fb100bbb37
Simon McConnell <22566656+simonmcconnell@users.noreply.github.com>
2024-06-28 09:54:19 +0800

    add job using hash

--- lib/recj.ex
+++ lib/recj.ex
@@ -30,6 +30,22 @@ defmodule Recj do
     |> Oban.insert()
   end
 
+  def create_job4(thing \\ "one") when thing in ~w[one two] do
+    tags = [
+      ["a", "1", "2"],
+      ["b", "1", "3"],
+      ["c", "1", "4"]
+    ]
+
+    %{
+      "thing" => thing,
+      "tags" => tags,
+      "hash" => :erlang.phash2(tags)
+    }
+    |> Recj.ThingDataJob4.new(schedule_in: 1)
+    |> Oban.insert()
+  end
+
   def reset do
     Recj.Repo.delete_all(Recj.ThingData)
     Recj.Repo.delete_all("oban_jobs")
